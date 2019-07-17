a7ae063c42a3a5576b7b5e1b93fec56b0aff3747
FlorainSoong <38419035+FlorainSoong@users.noreply.github.com>
2019-07-17 16:01:15 +0800

    Add files via upload

--- src/main/java/com/qienys/JnuPrac/service/OrderProductsService.java
+++ src/main/java/com/qienys/JnuPrac/service/OrderProductsService.java
@@ -1,4 +1,9 @@
 package com.qienys.JnuPrac.service;
 
+import com.qienys.JnuPrac.pojo.OrderProducts;
+
+import java.util.List;
+
 public interface OrderProductsService  {
+    List<OrderProducts> findByOrderId(Long orderId);
 }
