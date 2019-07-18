6d7bc74985f4ef33ed5e02e075238bcf3941bd77
FlorainSoong <38419035+FlorainSoong@users.noreply.github.com>
2019-07-18 11:35:10 +0800

    Add files via upload

--- src/main/java/com/qienys/JnuPrac/service/impl/OrdersServiceImpl.java
+++ src/main/java/com/qienys/JnuPrac/service/impl/OrdersServiceImpl.java
@@ -5,6 +5,7 @@ import com.qienys.JnuPrac.pojo.Orders;
 import com.qienys.JnuPrac.service.OrdersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 
@@ -14,8 +15,13 @@ public class OrdersServiceImpl implements OrdersService {
     private OrdersRepository ordersRepository;
 
     @Override
-    public List<Orders> findByUid(Long uid){
-        List<Orders> ordersList = ordersRepository.findByUid(uid);
+    public Orders findByUid(Long uid){
+        Orders orders = ordersRepository.findByUid(uid);
+        return orders;
+    }
+
+    public List<Orders> findAllByUid(Long uid){
+        List<Orders> ordersList = ordersRepository.findAllByUid(uid);
         return ordersList;
     }
 }
